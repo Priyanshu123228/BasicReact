1f9abcb9431268e55d001a5e2b37c5bb62dc3a3f
Priyanshu123228 <priyanshusaini1212@gmail.com>
2026-07-10 19:13:54 +0530

    MsgBox componet to display hello username

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import "./App.css"
 import Title from "./Title.jsx"
 import Product from "./Product.jsx";
 import ProductTab from "./ProductTab.jsx";
+import MsgBox from "./MsgBox.jsx";
 
-function Description() {
-  return (
-    <h1> but we Care</h1>
-  );
-}
+
+// function Description() {
+//   return (
+//     <h1> but we Care</h1>
+//   );
+// }
 
 function App() {
+
   return (
-   <ProductTab/>
+    <>
+      <MsgBox userName="Priyanshu " textColor="pink"/>
+      <MsgBox userName="Jp " textColor="yellow"/>
+      <MsgBox userName="khusal " textColor="blue"/>
+      <ProductTab />
+    </>
   );
+
 }
 export default App
